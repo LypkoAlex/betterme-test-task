7fffccb0709123b11afc8cb297641373f5678997
Lypko Oleksandr <aleksandr.lypko@piano.io>
2020-10-31 15:44:02 +0200

    added cookie parser. imported auth modele

--- src/repositories/controllers/repositories.controller.ts
+++ src/repositories/controllers/repositories.controller.ts
@@ -4,8 +4,11 @@ import { JoiValidationPipe } from '../../common/validation/validation.pipe';
 import { RepositoriesService }  from '../providers/repositories.service';
 import { SearchInput } from '../models/repositories.search.interface';
 import { SearchSchema } from '../models/repositories.search.schema';
+import { JwtAuthenticationGuard } from 'src/common/auth/auth.guard';
+import { UseGuards } from '@nestjs/common/decorators/core/use-guards.decorator';
 
 @Controller()
+@UseGuards(JwtAuthenticationGuard)
 export class GithubController {
     constructor(private repositoriesService: RepositoriesService) {};
 
--- src/repositories/repositories.module.ts
+++ src/repositories/repositories.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import { GithubController } from './controllers/repositories.controller';
 import { RepositoriesService } from './providers/repositories.service';
+import { AuthModule } from '../common/auth/auth.module';
 
 @Module({ 
     imports: [
@@ -10,6 +11,7 @@ import { RepositoriesService } from './providers/repositories.service';
         ConfigModule.forRoot({
             envFilePath: ['.env.development.local'],
         }),
+        AuthModule
     ],
     controllers: [GithubController],
     providers: [RepositoriesService],
